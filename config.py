20796c5ec601ac9be57510a19c45e25572a992c1
Gregory Andruschak <michael_kapitero@hotmail.com>
2018-04-27 07:21:09 +0300

    fix 9

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = 'mysql://admin:javaee2018!@sl-eu-lon-2-portal.7.dblayer.com:25192/Relative_Gravitas'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = 15
